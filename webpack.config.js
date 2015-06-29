6f7a2d6ffb36fe5b3ccfe563a0ac2fe167b92991
Jakub Złoczewski <jzlocz@gmail.com>
2015-06-29 21:31:00 +0200

    added eslint and webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/*global require, module, __dirname*/
+var path = require('path');
+var scriptsSrcPath = path.join(__dirname, 'frontend', 'scripts'),
+    dev = true;
+
+module.exports = {
+    cache: true,
+    watch: true,
+    devtool: dev ? 'source-map' : null,
+    entry: path.join(scriptsSrcPath, 'main.js'),
+    output: {
+        path: './public/',
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loader: 'style!css?localIdentName=' + ( dev ? '[name]__[local]___[hash:base64:5]' : '[hash:base64:5]' ) + '!less'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel?stage=0'
+            }
+        ]
+    }
+};
